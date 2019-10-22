d13e5e5ae86da73f9260812fa7aa92870091beed
jamiroq <jamiroq24@gmail.com>
2019-10-22 22:24:22 +0900

    [modify] Fix bug that failed to install using curl, wget

--- init.sh
+++ init.sh
@@ -114,13 +114,14 @@ dotfiles_download() {
 
     elif has "curl" || has "wget"; then
         local tarball="https://github.com/jamiroq/dotfiles/archive/master.tar.gz"
+        mkdir -p "$DOTPATH"
         if has "curl"; then
             curl -L "$tarball"
 
         elif has "wget"; then
             wget -O - "$tarball"
 
-        fi | tar xvz -C "$DOTPATH"
+        fi | tar xvz -C "$DOTPATH" --strip-components 1
 
     else
         fail "git or curl, wget required"
