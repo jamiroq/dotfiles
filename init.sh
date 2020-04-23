1c8d9087f0e73de8531e60ed06b1f1e9395c3982
jamiroq <jamiroq24@gmail.com>
2020-04-24 01:03:47 +0900

    [modify] Add package install function

--- init.sh
+++ init.sh
@@ -56,39 +56,9 @@ fail() {
     paint "$*\n" "red" "bold" 1>&2
 }
 
-# lower returns the string with all letters mapped to their lower case.
-lower() {
-    if [ $# -eq 0 ]; then
-        cat <&0
-    elif [ $# -eq 1 ]; then
-        if [ -f "$1" -a -r "$1" ]; then
-            cat "$1"
-        else
-            echo "$1"
-        fi
-    else
-        return 1
-    fi | tr "[:upper:]" "[:lower:]"
-}
-
-# upper returns the string with all letters mapped to their upper case.
-upper() {
-    if [ $# -eq 0 ]; then
-        cat <&0
-    elif [ $# -eq 1 ]; then
-        if [ -f "$1" -a -r "$1" ]; then
-            cat "$1"
-        else
-            echo "$1"
-        fi
-    else
-        return 1
-    fi | tr "[:lower:]" "[:upper:]"
-}
-
-#===========================================
-# Bootstrap_END }}}
 #-------------------------------------------
+# Bootstrap_END }}}
+#===========================================
 
 #===========================================
 # Initalize {{{
@@ -111,22 +81,17 @@ dotfiles_download() {
 
     if has "git"; then
         git clone --recursive "$DOTFILES_GITHUB" "$DOTPATH"
-
     elif has "curl" || has "wget"; then
         local tarball="https://github.com/jamiroq/dotfiles/archive/master.tar.gz"
         mkdir -p "$DOTPATH"
         if has "curl"; then
             curl -L "$tarball"
-
         elif has "wget"; then
             wget -O - "$tarball"
-
         fi | tar xvz -C "$DOTPATH" --strip-components 1
-
     else
         fail "git or curl, wget required"
         exit 1
-
     fi
 }
 
@@ -152,6 +117,46 @@ dotfiles_install() {
     dotfiles_deploy
 }
 
+homebrew_preinstall() {
+    info "Pre-installing..."
+
+    # For RedHat
+    if [ -f /etc/redhat-release ]; then
+        sudo yum groupinstall 'Development Tools' && \
+        sudo yum install curl file git
+
+    # For Debian
+    elif [ -f /etc/debian_version ]; then
+        sudo apt install build-essential curl file git
+
+    else
+        fail "OS not supported"
+        exit 1
+    fi
+}
+
+homebrew_install() {
+    echo "homebrew installing..."
+    local linuxbrew_installer="https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh"
+    sh -c "$(curl -fsSL $linuxbrew_installer)"
+    eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+}
+
+tools_install() {
+    brew upgrade
+    brew install vim
+    brew install zsh
+    brew install tmux
+    brew install ripgrep
+    brew install ghq
+}
+
+packages_install() {
+    homebrew_preinstall &&
+    homebrew_install &&
+    tools_install
+}
+
 #-------------------------------------------
 # Initialize_END }}}
 #===========================================
@@ -168,14 +173,15 @@ fi
 DOTFILES_GITHUB="https://github.com/jamiroq/dotfiles.git"
 export DOTFILES_GITHUB
 
-# [case] source xxxx.sh || [case] bash xxxx.sh
+# [case] source xx.sh || [case] bash xx.sh
 [[ $- = *i* || "$0" = "${BASH_SOURCE:-}" ]] && exit
 
-# [case] bash -c "$(cat a.sh)" || [case] cat a.sh | bash
+# [case] bash -c "$(cat xx.sh)" || [case] cat xx.sh | bash
 if [ -n "${BASH_EXECUTION_STRING:-}" ] || [ -p /dev/stdin ]; then
 
     trap catch INT ERR
     dotfiles_install
+    packages_install
 
     # Restart shell if specified "bash -c $(curl -L {URL})"
     # not restart:
